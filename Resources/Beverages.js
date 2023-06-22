7e42de911a3fadd8e4b3d7424cbf972f018111cc
haleomo <sommerkeit@hotmail.com>
2023-06-22 10:44:43 -1000

    inital upload of files

---
+++ Resources/Beverages.js
@@ -0,0 +1,79 @@
+var beverages = {allAgeBeverages : {
+    title: "All Ages",
+    products: [
+    {
+        "brand": "Tejava",
+        "product": "Black Ice Tea",
+        "description": "Black ice tea in a 12 ounce glass bottle."
+    },
+    {
+        "brand": "Big Island Organics",
+        "product": "Hawaiian Gingeraid",
+        "description": "Big Island Organics, Hawaiian Gingerade Is A Usda Certified Organic Drink. This Product Contains Powerful Antioxidants, Anti-inflammatory Agents, And Is Made With Fresh Pressed Ginger."
+    },
+    {
+        "brand": "Shaka Tea",
+        "product": "Pineapple Mint",
+        "description": "Shaka Tea is brewed with one of the rarest herbal teas in the world, only found in the Hawaiian Islands: māmaki. Pineapple Mint is a tangy take with a hint of unexpected, herbal and refreshing mint!"
+    },
+    {
+        "brand": "Shaka Tea",
+        "product": "Guava Ginger Blossum",
+        "description": "Shaka Tea is brewed with one of the rarest herbal teas in the world, only found in the Hawaiian Islands: māmaki. Enjoy tart guava with floral ginger blossoms."
+    },
+    {
+        "brand": "Sprite",
+        "product": "Zero -- Mini",
+        "description": "Sprite Zero Sugar is a diet Sprite with no sugar in a 7.5 ounce can. "
+    },
+    {
+        "brand": "Coke",
+        "product": "Zero -- Mini",
+        "description": "Enjoy the real taste of Coca-Cola in a zero calorie, sugar free beverage in a 7.5 ounce cans."
+    },
+    {
+        "brand": "Crystal Geyser",
+        "product": "Sparkling Spring Water Lime",
+        "description": "Crystal Geyser Alpine Sparkling Spring Water, Lime Is A Pure And Natural Water Option. With Light Flavor And Carbonation, Crystal Geyser Alpine Sparkling Spring Water Gives You A Refreshing And Elegant. This product is Kosher and available with a slice of real lime."
+     }
+    ]
+    },
+    adultBeverages: {
+        title: "21 and older",
+        products: [
+            {
+                "brand": "Maui Brewing Company",
+                "product": "Coconut Hiwa Porter",
+                "description": "A robust dark ale with hand-toasted coconut & hints of mocha in a 12 ounce can. "
+            },
+            {
+                "brand": "Maui Brewing Company",
+                "product": "Pacific Lite Session IPA (SIPA)",
+                "description": "Light and Crisp Session IPA in a 12 ounce can. "
+            },
+            {
+                "brand": "Maui Brewing Company",
+                "product": "Coconut Hiwa Porter",
+                "description": "A robust dark ale with hand-toasted coconut & hints of mocha in a 12 ounce can. "
+            },
+            {
+                "brand": "Maui Brewing Company",
+                "product": "Hard Seltzer - POG",
+                "description": "A Passion Organge Guava Selzer made from pure cane sugar in a 12 ounce can. "
+            },
+            {
+                "brand": "Maui Brewing Company",
+                "product": "Hard Seltzer - POG",
+                "description": "A Passion Organge Guava Selzer made from pure cane sugar in a 12 ounce can. "
+            },
+            {
+                "brand": "Waikiki Brewing Company",
+                "product": "Craft Lite",
+                "description": "Light lager that's crisp, clean and refreshing. Everything you want from a lager in a 12 ounce can. "
+            },
+
+
+    ]
+    }
+
+}
