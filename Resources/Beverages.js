1702fc1f613a5203b931b2d401d10129e5fc4dff
haleomo <sommerkeit@hotmail.com>
2023-07-06 16:52:46 -1000

    Updated the products and re-arranged the tabs so WiFi is first

--- Resources/Beverages.js
+++ Resources/Beverages.js
@@ -2,6 +2,16 @@ var beverages = {allAgeBeverages : {
     title: "All Ages",
     products: [
     {
+        "brand": "MeadowGold",
+        "product": "2% Milk",
+        "description": "Your go-to for goodness. It’s delicious and wholesome — so you’ll get all the nutrients of whole milk and less fat."
+    },
+    {
+        "brand": "Dairy Gold",
+        "product": "Old Fashion Chocolate Milk",
+        "description": "Richer and creamier than our low fat chocolate milk. Made with reduced fat milk, no artificial sweeteners, and real cocoa – this is milk you can feel good about."
+    },
+    {
         "brand": "Tejava",
         "product": "Black Ice Tea",
         "description": "Black ice tea in a 12 ounce glass bottle."
@@ -22,6 +32,11 @@ var beverages = {allAgeBeverages : {
         "description": "Shaka Tea is brewed with one of the rarest herbal teas in the world, only found in the Hawaiian Islands: māmaki. Enjoy tart guava with floral ginger blossoms."
     },
     {
+        "brand": "Coke",
+        "product": "Diet -- 12oz -- Optional Lemon Slice",
+        "description": "Enjoy the classic taste of Diet Coke in a 12 ounce can."
+    },
+    {
         "brand": "Sprite",
         "product": "Zero -- Mini",
         "description": "Sprite Zero Sugar is a diet Sprite with no sugar in a 7.5 ounce can. "
@@ -32,10 +47,30 @@ var beverages = {allAgeBeverages : {
         "description": "Enjoy the real taste of Coca-Cola in a zero calorie, sugar free beverage in a 7.5 ounce cans."
     },
     {
+        "brand": "Hawaiian Sun",
+        "product": "Lilikoi Lychee",
+        "description": "passion fruit juice and lychee flavor makes this a perfect choice for the beach or pool. Made in Hawaii."
+     },
+     {
+        "brand": "Hawaiian Sun",
+        "product": "Lilikoi Passion",
+        "description": "Lilikoi, also known as passion fruit, is used to create this tropically tart flavor. ask our many fans who are passionate about it!  Made in Hawaii."
+     },
+     {
+        "brand": "Hawaiian Sun",
+        "product": "Pass-O-Guava",
+        "description": "Passion fruit aka lilikoi, orange and guava juices are blended to create this tropical flavor that reminds us of hawaii. Made in Hawaii."
+     },
+    {
         "brand": "Crystal Geyser",
         "product": "Sparkling Spring Water Lime",
         "description": "Crystal Geyser Alpine Sparkling Spring Water, Lime Is A Pure And Natural Water Option. With Light Flavor And Carbonation, Crystal Geyser Alpine Sparkling Spring Water Gives You A Refreshing And Elegant. This product is Kosher and available with a slice of real lime."
-     }
+     },
+     {
+        "brand": "Signature",
+        "product": "Diet Ginger Ale",
+        "description": "Naturally flavored with other natural flavors. 0 calories per 12 fl oz. Calorie free. Caffeine free."
+     }    
     ]
     },
     adultBeverages: {
@@ -43,23 +78,13 @@ var beverages = {allAgeBeverages : {
         products: [
             {
                 "brand": "Maui Brewing Company",
-                "product": "Coconut Hiwa Porter",
-                "description": "A robust dark ale with hand-toasted coconut & hints of mocha in a 12 ounce can. "
-            },
-            {
-                "brand": "Maui Brewing Company",
-                "product": "Big Swell IPA",
-                "description": "Tropical citrus hops burst from this dry-hopped India Pale Ale in a 12 ounce can. "
-            },
-            {
-                "brand": "Maui Brewing Company",
                 "product": "Hard Seltzer - POG",
                 "description": "A Passion Organge Guava Selzer made from pure cane sugar in a 12 ounce can. "
             },
             {
-                "brand": "Waikiki Brewing Company",
-                "product": "Craft Lite",
-                "description": "Light lager that's crisp, clean and refreshing. Everything you want from a lager in a 12 ounce can. "
+                "brand": "Absolut Vodka",
+                "product": "Moscow Mule Cocktail",
+                "description": "Made with lime juice and ginger beer."
             }
     ]
     }
